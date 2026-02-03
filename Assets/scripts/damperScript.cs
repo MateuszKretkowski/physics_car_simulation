1ffb3f9f10cd7f8d7cefe7c711828e8ead42c055
Mateusz Kretkowski <mkretkowski15@gmail.com>
2026-02-03 22:17:46 +0100

    Added car, transformed damperScript.cs (not final, just uploading to not lose it).

--- Assets/scripts/damperScript.cs
+++ Assets/scripts/damperScript.cs
@@ -1,57 +1,99 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class damperScript : MonoBehaviour
 {
     public Spring spring;
 
     public GameObject targetPoint;
+    public GameObject car;
     public GameObject springBody;
-    
-    Rigidbody target_rb;
+
+    [SerializeField] Rigidbody target_rb;
+    [SerializeField] Rigidbody car_rb;
 
     [SerializeField] float energyLoss;
-    [SerializeField] float distance;
-    [SerializeField] Vector3 direction;
+    [SerializeField] float tireOffset;
+
+    [SerializeField] float tireDistance;
+    [SerializeField] float carDistance;
+    [SerializeField] Vector3 tireDirection;
+    [SerializeField] Vector3 carDirection;
 
     void Start()
     {
-        if (targetPoint && spring)
+        if (!car)
         {
+            car = transform.root.gameObject;
+        }
+        if (targetPoint && spring && car)
+        {
+            targetPoint = Instantiate(targetPoint, new Vector3(transform.position.x, transform.position.y + tireOffset, transform.position.z), Quaternion.identity);
             target_rb = targetPoint.GetComponent<Rigidbody>();
+            car_rb = car.GetComponent<Rigidbody>();
         }
     }
 
     void Update()
     {
-        CalculateDistance();
+        tireDirection = (transform.position - targetPoint.transform.position).normalized;
+        carDirection = Vector3.up;
+        tireDistance = CalculateDistance(transform.position + tireDirection * spring.relaxedDistance, targetPoint);
+        carDistance = CalculateDistance(transform.position + carDirection * spring.relaxedDistance, car);
         CalculateForces();
         CalculateSpringBodyContraction();
-    }   
+    }
 
-    void CalculateDistance()
+    float CalculateDistance(Vector3 self, GameObject target)
     {
-        direction = ((transform.position - targetPoint.transform.position) / (transform.position - targetPoint.transform.position).magnitude).normalized;
-        distance = Vector3.Distance(transform.position + direction * spring.relaxedDistance, targetPoint.transform.position);
+        return Vector3.Distance(self, target.transform.position);
     }
 
     void CalculateForces()
     {
-        // distance between two targets
-        if (distance >= spring.epsilon) 
+        Vector3 tireForce = CalculateTireForce();
+        Vector3 carForce = CalculateCarForce();
+
+        target_rb.AddForce(tireForce, ForceMode.Impulse);
+        car_rb.AddForceAtPosition(carForce, transform.position);
+    }
+
+    Vector3 CalculateTireForce()
+    {
+        if (tireDistance >= spring.epsilon)
         {
             Debug.Log("spring broke.");
-            return;
+            return Vector3.zero;
         }
-        // F = -kx
-        float force = spring.stiffness * Mathf.Pow(distance, 2) / 2;
+        float force = spring.stiffness * Mathf.Pow(tireDistance, 2) / 2;
         Debug.Log("force: " + force);
-        Debug.DrawRay(targetPoint.transform.position, direction, Color.red);
-        target_rb.AddForce(direction * distance * force * energyLoss, ForceMode.Impulse);
+        Debug.DrawRay(targetPoint.transform.position, tireDirection, Color.red);
+        return tireDirection * (tireDistance) * force / 2;
+    }
+
+    Vector3 CalculateCarForce()
+    {
+        if (carDistance >= spring.epsilon)
+        {
+            Debug.Log("spring broke: car too far.");
+            return Vector3.zero;
+        }
+        if (!car_rb || !car)
+        {
+            return Vector3.zero;
+        }
+        float force = spring.stiffness * Mathf.Pow(carDistance, 2) / 2;
+        float distanceToCar = CalculateDistance(transform.position, car);
+        carDirection.x *= 0f;
+        carDirection.z *= 0f;
+        // force = spring.stiffness * Mathf.Pow((distance + distanceToCar), 2);
+        Debug.DrawRay(transform.position, -carDirection, Color.pink);
+        return -carDirection * (carDistance) * force / 2;
     }
 
     void CalculateSpringBodyContraction()
     {
-        Vector3 newScale = new Vector3(springBody.transform.localScale.x, springBody.transform.localScale.y, distance);
+        Vector3 newScale = new Vector3(springBody.transform.localScale.x, springBody.transform.localScale.y, tireDistance);
         springBody.transform.localScale = newScale;
         springBody.transform.LookAt(targetPoint.transform.position);
     }
