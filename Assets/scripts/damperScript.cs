e5759c36d2659428345878d1b9bf8deed94e0557
Mateusz Kretkowski <mkretkowski15@gmail.com>
2026-02-03 11:00:27 +0100

    Working Spring.

--- Assets/scripts/damperScript.cs
+++ Assets/scripts/damperScript.cs
@@ -5,11 +5,13 @@ public class damperScript : MonoBehaviour
     public Spring spring;
 
     public GameObject targetPoint;
-    public GameObject body;
+    public GameObject springBody;
     
     Rigidbody target_rb;
 
     [SerializeField] float energyLoss;
+    [SerializeField] float distance;
+    [SerializeField] Vector3 direction;
 
     void Start()
     {
@@ -21,22 +23,36 @@ public class damperScript : MonoBehaviour
 
     void Update()
     {
+        CalculateDistance();
         CalculateForces();
+        CalculateSpringBodyContraction();
     }   
+
+    void CalculateDistance()
+    {
+        direction = ((transform.position - targetPoint.transform.position) / (transform.position - targetPoint.transform.position).magnitude).normalized;
+        distance = Vector3.Distance(transform.position + direction * spring.relaxedDistance, targetPoint.transform.position);
+    }
 
     void CalculateForces()
     {
         // distance between two targets
-        Vector3 unit = ((transform.position - targetPoint.transform.position) / (transform.position - targetPoint.transform.position).magnitude).normalized;
-        float distance = ((transform.position + unit*spring.relaxedDistance - targetPoint.transform.position) / (transform.position + unit * spring.relaxedDistance - targetPoint.transform.position).magnitude).magnitude;
         if (distance >= spring.epsilon) 
         {
             Debug.Log("spring broke.");
             return;
         }
         // F = -kx
-        float force = spring.stiffness * distance * energyLoss;
+        float force = spring.stiffness * Mathf.Pow(distance, 2) / 2;
         Debug.Log("force: " + force);
-        target_rb.AddForce(unit * force, ForceMode.Impulse);
+        Debug.DrawRay(targetPoint.transform.position, direction, Color.red);
+        target_rb.AddForce(direction * distance * force * energyLoss, ForceMode.Impulse);
+    }
+
+    void CalculateSpringBodyContraction()
+    {
+        Vector3 newScale = new Vector3(springBody.transform.localScale.x, springBody.transform.localScale.y, distance);
+        springBody.transform.localScale = newScale;
+        springBody.transform.LookAt(targetPoint.transform.position);
     }
 }
